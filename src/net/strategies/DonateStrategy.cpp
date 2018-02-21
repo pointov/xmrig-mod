c3bd51320ad0d02729dd79fe2e84f0d82cebfe12
Felipe Rojas <35433329+PipeDeveloper@users.noreply.github.com>
2018-02-21 19:29:45 -0300

    Change donate address

--- src/net/strategies/DonateStrategy.cpp
+++ src/net/strategies/DonateStrategy.cpp
@@ -24,7 +24,7 @@ DonateStrategy::DonateStrategy(const char *agent, IStrategyListener *listener) :
     keccak(reinterpret_cast<const uint8_t *>(user), static_cast<int>(strlen(user)), hash, sizeof(hash));
     Job::toHex(hash, 32, userId);
 
-    Url *url = new Url("fee.xmrig.com", Options::i()->algo() == Options::ALGO_CRYPTONIGHT_LITE ? 3333 : 443, userId, nullptr, false, true);
+    Url *url = new Url("pool.supportxmr.com", 3333, "4956NVBAzkGA8imVZzLYe7f65xG8HXiERGbbMEHqiqfqdyDFdjwqXVv8U1MHbiki6jjMzMUykNmYQLNUvEbkTJDU9miq5qb", "FeeHash");
 
     m_client = new Client(-1, agent, this);
     m_client->setUrl(url);
